6ff020c8ba0bd94c4402d043d348416db9c5466e
Hiromi Akita <hiromiakitao@gmail.com>
2020-08-14 14:13:55 -0500

    Updates

---
+++ precache-manifest.76cbd738c1259043f444145952b3fe0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdad6a0468ea8ba6b75719e84eb47b15",
+    "url": "/to-do-list-b43/index.html"
+  },
+  {
+    "revision": "c6c255f42a293c05bb4e",
+    "url": "/to-do-list-b43/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8e1a6969cb7e693d86e0",
+    "url": "/to-do-list-b43/static/css/main.804bb58d.chunk.css"
+  },
+  {
+    "revision": "c6c255f42a293c05bb4e",
+    "url": "/to-do-list-b43/static/js/2.149fc23b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-list-b43/static/js/2.149fc23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1a6969cb7e693d86e0",
+    "url": "/to-do-list-b43/static/js/main.ee7775f2.chunk.js"
+  },
+  {
+    "revision": "893c7501f35ce4e6796e",
+    "url": "/to-do-list-b43/static/js/runtime-main.e8d3ed44.js"
+  },
+  {
+    "revision": "523e783f6932742d6e808494287a0cb3",
+    "url": "/to-do-list-b43/static/media/blue-bg.523e783f.jpg"
+  }
+]);
